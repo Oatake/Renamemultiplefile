954a91140d5ae96dd3e5d978a3edcbd5ce926866
Oatake <Thitiwut.ekwisit@gmail.com>
2022-06-04 23:40:52 +0700

    Add feature copy to another directory and rename it

--- RenameFile.py
+++ RenameFile.py
@@ -1,4 +1,5 @@
 import os
+import shutil #library for copy file command
 
 path = "C:\\Users\\Oat\\Documents\\Project\\RenameFile\\Example\\"
 outputpath = "C:\\Users\\Oat\\Documents\\Project\\RenameFile\\Output\\"
@@ -7,6 +8,6 @@ print("Hello world")
 print(f"Before Renaming: {os.listdir(path)}")
 for i in os.listdir(path):
     print(i)
-    os.rename(path+i, path+i+'.txt')
+    shutil.copyfile(path+i, outputpath+i)               #copy file from example to output
+    os.rename(outputpath+i, outputpath+i+'.txt')        #rename file by adding .txt
 #print(f"After Renaming: {os.listdir(path)}")
-
