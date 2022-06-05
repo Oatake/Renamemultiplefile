b125523e7d07f64784d9a5a9c016bfb2480e5d76
Oatake <Thitiwut.ekwisit@gmail.com>
2022-06-05 22:36:47 +0700

    Add try/except on remove and create directory to prevent OS error

--- RenameFile.py
+++ RenameFile.py
@@ -4,8 +4,18 @@ import shutil #library for copy file command
 path = "C:\\Users\\Oat\\Documents\\Project\\RenameFile\\Example\\"
 outputpath = "C:\\Users\\Oat\\Documents\\Project\\RenameFile\\Output\\"
 
-shutil.rmtree(outputpath)                               #remove non-empty directory
-os.mkdir(outputpath)
+try :
+    shutil.rmtree(outputpath)                               #remove non-empty directory
+except OSError as e:
+    print("Error cannot remove directory : %s : %s" % (outputpath, e.strerror))
+# try :    
+#     os.mkdir(outputpath)    
+# except OSError as e:                                #create directory to be output file directory
+#     print("Error cannot create directory : %s : %s" % (outputpath, e.strerror))
+
+
+# shutil.rmtree(outputpath)   
+os.mkdir(outputpath)                                #remove non-empty directory
 
 print(f"Before Renaming: {os.listdir(path)}")
 for i in os.listdir(path):
